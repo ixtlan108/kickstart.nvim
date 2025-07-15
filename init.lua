037fe1117a2e5a84aeb97daf8aa12d080a4cd90c
Hamilton108 <hamilton108@klaxton.no>
2025-07-15 16:32:00 +0200

    Catppuccin and Lualine

--- init.lua
+++ init.lua
@@ -887,7 +887,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      --vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
       -- vim.cmd('hi TabLine guibg=#4682b4')
     end,
   },
